a835cb28608c28a2b63522526de6da1c20ea188d
Khadkashankar <syamd5421@gmail.com>
2023-06-14 23:18:30 +0545

    added authenicate in admin

--- app/Http/Middleware/AdminRedirectIfAuthenticated.php
+++ app/Http/Middleware/AdminRedirectIfAuthenticated.php
@@ -6,9 +6,10 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 use Symfony\Component\HttpFoundation\Response;
 
-class RedirectIfAuthenticated
+class AdminRedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
@@ -17,7 +18,7 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
-            if (Auth::guard($guard)->check()) {
+            if (Auth::guard('admin')->check()) {
                 // return redirect(RouteServiceProvider::HOME);
             
         }
