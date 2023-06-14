4d3464349fd4162aefa49681be1aebdb310fb329
Khadkashankar <syamd5421@gmail.com>
2023-06-14 23:20:33 +0545

    added authenticate

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
@@ -11,4 +12,22 @@ class AdminLoginController extends Controller
     public function index(){
         return view('admin.login');
     }
+
+    public function Authenticate(Request $request)
+   {
+       $validator = Validator::make($request->all(),[
+        'email' => 'required|email',
+        'password'=> 'required'
+       ]);
+
+       if($validator->passes()){
+
+       }
+       else{
+        return redirect()->route('admin.login')
+        ->withErrors($validator)
+        ->withInput($request->only('email'));
+       }
+  } 
+
 }
